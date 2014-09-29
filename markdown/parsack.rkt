56e10dbf7125a67c60b6ed619363b36f72e798cc
Leif Andersen <leif@leifandersen.net>
2014-09-29 18:22:29 -0400

    Matches the new API for parsack features.

--- markdown/parsack.rkt
+++ markdown/parsack.rkt
@@ -27,8 +27,8 @@
                   getState setState withState
                   State State? Consumed Consumed! Empty Ok Error Msg
                   parse parse-result parsack-error parse-source
-                  incr-pos
-                  parsack-features))
+                  incr-pos)
+         parsack/features)
 
 (provide pdo
          pdo-one
